d6cc3e53bead9aef67a758d565971081b8254a77
Misa <misaelvelazquez0227@Gmail.com>
2025-07-20 21:43:24 -0700

    GOD3

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,7 +1,35 @@
- export const BASE_URL = "";
+ //export const BASE_URL = "";
 
-export const USERS_URL = "/api/users";
-export const PRODUCTS_URL = "/api/products";
-export const ORDERS_URL = "/api/orders";
-export const CATEGORY_URL = "/api/category";
-export const UOM_URL = "/api/uom";
+//export const USERS_URL = "/api/users";
+//export const PRODUCTS_URL = "/api/products";
+//export const ORDERS_URL = "/api/orders";
+//export const CATEGORY_URL = "/api/category";
+//export const UOM_URL = "/api/uom";
+
+
+
+
+
+//export const BASE_URL = import.meta.env.VITE_API_URL || "";
+
+//export const USERS_URL = `${BASE_URL}/api/users`;
+//export const PRODUCTS_URL = `${BASE_URL}/api/products`;
+//export const ORDERS_URL = `${BASE_URL}/api/orders`;
+//export const CATEGORY_URL = `${BASE_URL}/api/category`;
+//export const UOM_URL = `${BASE_URL}/api/uom`;
+
+
+//export const BASE_URL = import.meta.env.VITE_API_URL || "";
+
+//console.log("✅ BASE_URL:", BASE_URL); // <--- ADD THIS
+
+
+export const BASE_URL = process.env.REACT_APP_API_URL || "";
+console.log("✅ BASE_URL:", BASE_URL);
+
+
+export const USERS_URL = `${BASE_URL}/api/users`;
+export const PRODUCTS_URL = `${BASE_URL}/api/products`;
+export const ORDERS_URL = `${BASE_URL}/api/orders`;
+export const CATEGORY_URL = `${BASE_URL}/api/category`;
+export const UOM_URL = `${BASE_URL}/api/uom`;
